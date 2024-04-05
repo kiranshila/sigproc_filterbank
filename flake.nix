bb400e412863d3de2584d87384dd0dc1ccc2e74c
Kiran Shila <me@kiranshila.com>
2024-04-05 14:31:08 -0700

    nixify, bump versions

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {inherit system overlays;};
+
+      runCiLocally = pkgs.writeScriptBin "ci-local" ''
+        echo "Checking Rust formatting..."
+        cargo fmt --check
+
+        echo "Checking clippy..."
+        cargo clippy --all-targets
+
+        echo "Testing Rust code..."
+        cargo test
+      '';
+
+      nativeBuildInputs = with pkgs; [];
+      buildInputs =
+        [runCiLocally]
+        ++ (with pkgs; [
+          # Rust stuff, some stuff dev-only
+          (rust-bin.nightly.latest.default.override {
+            extensions = ["rust-src" "rust-analyzer"];
+          })
+
+          # Linting support
+          alejandra
+        ]);
+    in
+      with pkgs; {
+        devShells.default = mkShell {inherit buildInputs nativeBuildInputs;};
+      });
+}
